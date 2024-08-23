ae94ed39f3e9fb283fea07f57692009264a6d55f
Samet Orbuk <orbuk60@gmail.com>
2024-08-23 20:01:21 +0300

    T8: Sign Up User Form Page Fixes #10

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,9 @@
+export default function SignUpPage() {
+  return (
+    <>
+      <main className="bg-[#1F485B] bg-opacity-85 flex w-screen h-screen justify-center items-center px-[9rem] py-[3rem]">
+        <div className="shine-border w-full bg-[#1F485B] h-full"></div>
+      </main>
+    </>
+  );
+}
